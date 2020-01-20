8368844de7a65f031fd84962f542ff5bf729e2e3
Nicolas Tobalina <nicolas.tobalina@gmail.com>
2020-01-20 14:24:15 +0100

    Initial commit

---
+++ src/main/java/com/nick/twitapi/TwitapiApplication.java
@@ -0,0 +1,13 @@
+package com.nick.twitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nick/twitapi/TwitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.twitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
